a66ae85f82673c2fd8925034fdb2e8fa44ae20ab
jxwyxccs <jxwyxccs@gmail.com>
2018-08-15 12:23:57 +0800

    新增了带参数的view以及渲染template的view

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+#创建视图
+def index(request):
+    return HttpResponse("myApp index")
+
+#带参数的视图
+def detail(request,s):
+    return HttpResponse("detail - %s" % s)
+
+from .models import Grades,Students
+#渲染template的视图
+def grades(request):
+    #查询数据
+    gradesList = Grades.objects.all()
+    #将数据传递给模板
+    return render(request,"myApp/grades.html",{"grades":gradesList})
--- project/project/urls.py
+++ project/project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    #会将其他请求映射到myApp.urls下的文件
+    path("",include("myApp.urls"))
 ]
